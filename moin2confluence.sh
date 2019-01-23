cf9aca4209a1ee16e5a1145b3c03c4f6c7e80a16
Sarel Botha <sarel@botha.us>
2019-01-22 23:07:29 -0500

    Fix formatting issues

--- moin2confluence.sh
+++ moin2confluence.sh
@@ -27,10 +27,18 @@ cp $MOINROOT/data/pages/$PAGENAME/revisions/$LATEST_REV $WORKING/$PAGENAME.wiki
 # the moinmoin format is very similar to mediawiki
 # here we convert a few things that are different
 sed -i "s/^ \*/*/g" "$WORKING/$PAGENAME.wiki"
+sed -i "s/^  \*/**/g" "$WORKING/$PAGENAME.wiki"
+sed -i "s/^   \*/***/g" "$WORKING/$PAGENAME.wiki"
 sed -i "s/^ 1. /# /g" "$WORKING/$PAGENAME.wiki"
 sed -i "s/^  1. /## /g" "$WORKING/$PAGENAME.wiki"
 sed -i "s/{{{/<pre>/g" "$WORKING/$PAGENAME.wiki"
 sed -i "s/}}}/<\/pre>/g" "$WORKING/$PAGENAME.wiki"
+
+# remove ''''''
+sed -i "s/''''''//g" "$WORKING/$PAGENAME.wiki"
+
+# Start table handling
+# This table handling is a hack because we actually convert straight to confluence format and not mediawiki format
 
 # handle 8 column tables
 sed -i "s/^||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><td>\4<\/td><td>\5<\/td><td>\6<\/td><td>\7<\/td><td>\8<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
@@ -58,6 +66,8 @@ perl -0777 -i -pe 's/[^>]\r\n<tr>/\r\n<table>\r\n<tr>/g' "$WORKING/$PAGENAME.wik
 
 # add </table> to end of table
 perl -0777 -i -pe 's/<\/tr>\r\n[^<]/<\/tr>\r\n<\/table>/g' "$WORKING/$PAGENAME.wiki"
+
+# end table handling
 
 # convert from mediawiki to markdown
 pandoc -f mediawiki -t markdown -s "$WORKING/$PAGENAME.wiki" -o "$WORKING/$PAGENAME.md"
